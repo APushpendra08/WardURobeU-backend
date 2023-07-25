c8bbb080ee957034a0ea3c6b8cf860cb13e4bf0b
A Pushpendra <aladupushpendra08@gmail.com>
2023-07-26 00:34:19 +0530

    Update push.js
    Added message sending endpoint

--- routes/push.js
+++ routes/push.js
@@ -1,8 +1,15 @@
 const pushRouter = require('express').Router()
 const { default: mongoose } = require('mongoose')
 const tokenSchema = require("../schema/tokenSchema")
+const router = require('./ping')
+const admin = require('firebase-admin')
+require('dotenv').config()
 
 const tokenModel = mongoose.model('tokens', tokenSchema)
+
+// admin.initializeApp({
+//     credential: admin.credential.cert(JSON.parse(process.env.SERVICE_KEY))
+// })
 
 
 pushRouter.get("/", (req, res) => {
@@ -33,5 +40,55 @@ pushRouter.post("/addToken", (req, res) => {
         res.send(tokenAddRes)
     }).catch(e => res.send(e))
 })
+
+pushRouter.get("/sendMessage", async (req, res) => {
+
+    let title = req.query.title
+    let pushMessage = req.query.message
+
+    await sendPushMessage(title, pushMessage, res)
+})
+
+pushRouter.post("/sendMessage", async (req, res) => {
+
+    let title = req.body.title
+    let pushMessage = req.body.message
+
+    await sendPushMessage(title, pushMessage, res)
+})
+
+async function sendPushMessage(title, pushMessage, res){
+    await tokenModel.find().then( async (result) =>  {
+        console.log(result)
+        
+        let tokens = []
+        for(let i = 0; i < result.length; ++i){
+            let token = result[i].token
+            if(token != null)
+            tokens.push(token)
+        }
+
+        console.log(tokens)
+
+        let message = {
+            data: {
+                "title":title.toString(),
+                "message":pushMessage.toString()
+            },
+            tokens: tokens
+        }
+
+        await admin.messaging().sendMulticast(message).then((fcmRes) => {
+            console.log("FCM send result : " + fcmRes)
+            res.send(fcmRes)
+        }).catch((e) => {
+            console.log("FCM send error : " + e)
+            res.send(e)
+        })
+    }).catch((e) => {
+        console.log("Error in Mongo fetch " + e)
+        res.send(e)
+    })
+}
 
 module.exports = pushRouter
