ab032187488e0a0fe1f124108d30c19713f7a91f
A Pushpendra <aladupushpendra08@gmail.com>
2023-07-28 00:39:21 +0530

    endpoint for stale tokens added, logic for separating stale and fresh token added

--- routes/push.js
+++ routes/push.js
@@ -1,16 +1,12 @@
 const pushRouter = require('express').Router()
-const { default: mongoose } = require('mongoose')
+const { default: mongoose, Model } = require('mongoose')
 const tokenSchema = require("../schema/tokenSchema")
 const router = require('./ping')
 const admin = require('firebase-admin')
 require('dotenv').config()
 
 const tokenModel = mongoose.model('tokens', tokenSchema)
-
-// admin.initializeApp({
-//     credential: admin.credential.cert(JSON.parse(process.env.SERVICE_KEY))
-// })
-
+const uninstalledTokenModel = mongoose.model('uninstalled_tokens', tokenSchema)
 
 pushRouter.get("/", (req, res) => {
     res.send("Push endpoint")
@@ -19,7 +15,7 @@ pushRouter.get("/", (req, res) => {
 pushRouter.get("/tokens", async (req, res) => {
     // return the list of FCM Tokens
     let tokensData = await tokenModel.find()
-    res.send(tokensData)
+    res.send({"count":tokensData.length, tokensData})
 })
 
 pushRouter.post("/addToken", (req, res) => {
@@ -39,6 +35,19 @@ pushRouter.post("/addToken", (req, res) => {
         console.log(tokenAddRes)
         res.send(tokenAddRes)
     }).catch(e => res.send(e))
+})
+
+pushRouter.get("/uninstalled", async (req, res) => {
+    let uninstalledTokenDocs = await uninstalledTokenModel.find()
+    res.send({"response" : uninstalledTokenDocs})
+})
+
+pushRouter.get("/dropCollection", async (req, res) => {
+    let collectionName = req.query.collection
+    await mongoose.connection.db.dropCollection(collectionName).then((result) => {
+        res.send(result)
+    })
+    // res.send(collectionName)
 })
 
 pushRouter.get("/sendMessage", async (req, res) => {
@@ -78,8 +87,11 @@ async function sendPushMessage(title, pushMessage, res){
             tokens: tokens
         }
 
-        await admin.messaging().sendMulticast(message).then((fcmRes) => {
-            console.log("FCM send result : " + fcmRes)
+        await admin.messaging().sendMulticast(message).then(async (fcmRes) => {
+            console.log("FCM send result : " + fcmRes["responses"])
+
+            await trackUninstall(tokens, fcmRes["responses"])
+
             res.send(fcmRes)
         }).catch((e) => {
             console.log("FCM send error : " + e)
@@ -89,6 +101,53 @@ async function sendPushMessage(title, pushMessage, res){
         console.log("Error in Mongo fetch " + e)
         res.send(e)
     })
+}
+
+async function trackUninstall(tokenList, fcmRes){
+    let fcmListSize = fcmRes.length
+    let uninstalledTokens = []
+
+    for(let i = 0; i < fcmListSize; ++i){
+        if(fcmRes[i]["success"] == false){
+            uninstalledTokens.push(tokenList[i])
+        }
+    }
+
+    // console.log("uninstalled tokens" + uninstalledTokens)
+
+    let uninstallTokenCount = uninstalledTokens.length
+
+    for(let j = 0; j < uninstallTokenCount; ++j){
+        let uninstallToken = new uninstalledTokenModel({token: uninstalledTokens[j], timestamp: "", service: "FCM"})
+        uninstallToken.save().then(async (res) => {
+            console.log("Uninstall Token added : " + uninstalledTokens[j])
+            await dropStaleToken(uninstalledTokens[j])
+        }).catch(e => console.log("Error while adding uninstall token : " + uninstalledTokens[j]))
+    }
+}
+
+async function dropStaleToken(token){
+
+    console.log("token" + token)
+    // await tokenModel.find({token}).then((res) => {
+    //     console.log("token found " + res)
+    // })
+
+    await tokenModel.findOneAndDelete({token}).then((res) => {
+        console.log("Token is deleted")
+        console.log(res)
+    }).catch(e => console.log("Error " + e))
+
+    // console.log(token)
+    // await tokenModel.find({token: token}).then((res) => {
+    //     console.log(res)
+    //     console.log("Mama Mia")
+    // })
+
+    // await tokenModel.deleteOne({token}).then((res) => {
+    //     console.log(res)
+    //     console.log("Token drop : ")
+    // }).catch(e => console.log("Token drop failed " + e))
 }
 
 module.exports = pushRouter
